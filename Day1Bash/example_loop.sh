299fdef72255b3d06b99679fd20f77c70baf7666
AstroCom <astrocom@Administrators-MacBook-Pro-3.local>
2018-05-29 15:24:07 -0400

    committing only the loop portion

--- Day1Bash/example_loop.sh
+++ Day1Bash/example_loop.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-# I am creating a variable greeting
-
-
-greeting='Hello World'
-
-# I am printing the contents of greeting
-
-echo $greeting
-
 # touch and print a range of files in a loop
 
-for myfile in {A..Z}.tx;
+for myfile in {A..Z}.txt;
 do
     touch $myfile
     echo $myfile
