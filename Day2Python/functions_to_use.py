fea96677f5e6be2b96254bf62cdcdcb69cb466ad
AstroCom <astrocom@Administrators-MacBook-Pro-3.local>
2018-05-30 15:22:17 -0400

    adding functions to use

---
+++ Day2Python/functions_to_use.py
@@ -0,0 +1,9 @@
+'''This module contains some functions we will create.'''
+
+def division_function_only_float (dividend, divisor) :
+    '''create instructions that always returns the float value of dividend/ divisor'''    
+    divisor = divisor * 1.0
+    return dividend/divisor 
+
+#Example attribute
+example_dividend = 300
